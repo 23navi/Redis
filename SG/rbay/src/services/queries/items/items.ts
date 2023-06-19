1e35544283f9def965b89cab7da7d31d2a77998b
23navi <navisureka23@gmail.com>
2023-06-20 00:52:27 +0530

    get item and set item

--- SG/rbay/src/services/queries/items/items.ts
+++ SG/rbay/src/services/queries/items/items.ts
@@ -1,7 +1,24 @@
 import type { CreateItemAttrs } from '$services/types';
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { deserialize } from './deserialize';
+import { itemsKey } from '$services/keys';
+import { genId } from '$services/utils';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id));
+
+	if (Object.keys(item).length === 0) {
+		return null;
+	}
+
+	return deserialize(id, item);
+};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const itemId = genId();
+	await client.hSet(itemsKey(itemId), serialize(attrs));
+	return itemId;
+};
