51e15f73914c4e04659366f4f847adaf51ad69bb
23navi <navisureka23@gmail.com>
2023-06-26 15:11:33 +0530

    item view counter

--- SG/rbay/src/services/queries/items/items.ts
+++ SG/rbay/src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { deserialize } from './deserialize';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
 import { genId } from '$services/utils';
 
 export const getItem = async (id: string) => {
@@ -19,7 +19,6 @@ export const getItems = async (ids: string[]) => {
 	const getCommands = ids.map((id) => {
 		return client.hGetAll(itemsKey(id));
 	});
-	
 
 	const pipelineResults = await Promise.all(getCommands);
 
@@ -34,5 +33,9 @@ export const getItems = async (ids: string[]) => {
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const itemId = genId();
 	await client.hSet(itemsKey(itemId), serialize(attrs));
+	await client.zAdd(itemsByViewsKey(), {
+		value: itemId,
+		score: 0
+	});
 	return itemId;
 };
--- SG/rbay/src/services/queries/views.ts
+++ SG/rbay/src/services/queries/views.ts
@@ -1,1 +1,8 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { client } from '$services/redis';
+export const incrementView = async (itemId: string, userId: string) => {
+	await Promise.all([
+		client.hIncrBy(itemsKey(itemId), 'views', 1),
+		client.zIncrBy(itemsByViewsKey(), 1, itemId)
+	]);
+};
