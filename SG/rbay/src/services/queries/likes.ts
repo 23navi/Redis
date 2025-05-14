16119a9570e6d40cae78b929a8e96b3cceedd364
23navi <navisureka23@gmail.com>
2025-05-14 15:41:56 +0530

    add description to existing commands

--- SG/rbay/src/services/queries/likes.ts
+++ SG/rbay/src/services/queries/likes.ts
@@ -8,19 +8,24 @@ export const userLikesItem = async (itemId: string, userId: string) => {
 	return isLiked;
 };
 
+// Get all the items liked by an user with id (userId)
 export const likedItems = async (userId: string) => {
 	const ids = await client.sMembers(userLikesKey(userId));
 	const items = await getItems(ids);
 	return items;
 };
 
+// When user likes an item, we do two things, 1) Add the item to user liked items set. 2) Increase the counter of likes on the item's hash
 export const likeItem = async (itemId: string, userId: string) => {
 	const liked = await client.sAdd(userLikesKey(userId), itemId);
+	// We only increment the like counter if user didn't already liked the post. (Note: using sAdd helped us a lookup as if we try to add duplicate like, it will return 0)
 	if (liked) {
 		await client.hIncrBy(itemsKey(itemId), 'likes', 1);
 	}
 };
 
+
+// Opposite of like
 export const unlikeItem = async (itemId: string, userId: string) => {
 	const unlike = await client.sRem(userLikesKey(userId), itemId);
 	if (unlike) {
@@ -28,6 +33,8 @@ export const unlikeItem = async (itemId: string, userId: string) => {
 	}
 };
 
+
+// Given two users, we will find the intersection of their liked items.
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
 	const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
 	return getItems(ids);
