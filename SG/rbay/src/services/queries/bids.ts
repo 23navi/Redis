7b78b50e3d82b71bcb185d6e4f589077681affd4
23navi <navisureka23@gmail.com>
2025-05-17 23:05:03 +0530

    add bid graph functionality

--- SG/rbay/src/services/queries/bids.ts
+++ SG/rbay/src/services/queries/bids.ts
@@ -3,12 +3,22 @@ import { client } from '$services/redis';
 import { DateTime } from 'luxon';
 import { bidHistoryKey } from '$services/keys';
 
-export const createBid = async (attrs: CreateBidAttrs) => { };
+export const createBid = async (attrs: CreateBidAttrs) => {
+	// atters.createdAt is an instance of luxon datetime, that's why we have .toMillis() on it
+	const serializedBid = serializeHistory(attrs.amount, attrs.createdAt.toMillis())
 
-export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	// Adding the bid to the right of the list, new bids goes to the right
+	return client.rPush(bidHistoryKey(attrs.itemId), serializedBid)
 };
 
+export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
+	const startIndex = ((-1 * count) - offset);
+	const endIndex = (-1 - offset)
+
+	const bids = (await client.lRange(bidHistoryKey(itemId), startIndex, endIndex))
+		.map((element) => deserializeHistory(element));
+	return bids;
+};
 
 
 const serializeHistory = (amount: number, createdAt: number) => {
