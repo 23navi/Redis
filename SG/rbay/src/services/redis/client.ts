89ca8e5f3ec486470e36a267985957c3eae95655
23navi <navisureka23@gmail.com>
2025-05-23 22:52:22 +0530

    add example lua script with node redis

--- SG/rbay/src/services/redis/client.ts
+++ SG/rbay/src/services/redis/client.ts
@@ -1,14 +1,40 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT:
+				`
+				local key = KEYS[1]
+				local count = tonumber(ARGV[1])
+				for i = 1, count do
+					redis.call('INCR', key)
+				end
+				return redis.call('GET', key)
+			`,
+			transformArguments(key: string, incrementCount: number) {
+				return [key, incrementCount.toString()];
+			},
+			transformReply(reply: string) {
+				return parseInt(reply, 10);
+			}
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
 client.connect();
+
+client.on("connect", async () => {
+	const reply = await client.addOneAndStore("books:count", 5)
+	const result = await client.get("books:count")
+	console.log({ reply, result })
+})
 
 export { client };
