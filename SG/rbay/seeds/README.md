5dd7e7fec44f833d24eb33d992c828982921e2a1
23navi <navisureka23@gmail.com>
2025-06-03 10:47:30 +0530

    add seed data to redis server

---
+++ SG/rbay/seeds/README.md
@@ -0,0 +1,11 @@
+## Using the seeds
+
+The script is created to seed in some random data to our redis server
+
+To run it
+
+```bash
+node ./seeds/seed.js
+```
+
+Note: This will empty the redis before seeding, so all existing data will be lost.
--- SG/rbay/src/services/queries/items/search.ts
+++ SG/rbay/src/services/queries/items/search.ts
@@ -1,1 +1,6 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+
+// This function will be called everytime user added any character in the search feild, think that input feild as controled component in react and for each keystroke, we are calling this function and updating the state of suggesition. (There is 500ms debounce too)
+// size is the number of search results to return, default to 5, we can send less if we don't have 5 matching results. 5 is the upper cap
+export const searchItems = async (term: string, size: number = 5) => {
+    console.log("This is called with: ", term)
+};
