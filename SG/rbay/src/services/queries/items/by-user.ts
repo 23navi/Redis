c7b8424b13d9e50f41a5809eecf8d1b61f1b4d81
23navi <navisureka23@gmail.com>
2025-06-08 20:36:21 +0530

    add user listing

--- SG/rbay/src/services/queries/items/by-user.ts
+++ SG/rbay/src/services/queries/items/by-user.ts
@@ -5,4 +5,13 @@ interface QueryOpts {
 	direction: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+// This endpoint is used by dashboard page to show the items that are placed for bid by the currently logged in user.
+export const itemsByUser = async (userId: string, opts: QueryOpts) => {
+	return []
+};
+
+
+
+// We will be producing the table with (item name, price, time left, no. of bids, no. of views, no. of likes and hyperlink to the bid)
+
+// Note: Redisearch can do sorting for us on hash field but only one field at a time and we have to tell which fileds are sortable when creating the index for that hash items
